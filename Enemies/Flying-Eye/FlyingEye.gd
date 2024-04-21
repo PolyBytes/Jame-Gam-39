04f1533467fee64b827657207fa7adc1861bc0ac
PolyBytes <drtylero80@gmail.com>
2024-04-21 18:37:15 -0400

    Implemented Basic AI for Flying Eye

--- Enemies/Flying-Eye/FlyingEye.gd
+++ Enemies/Flying-Eye/FlyingEye.gd
@@ -1,6 +1,16 @@
 extends CharacterBody2D
 
-var movement_speed: float = 300
+var movement_speed: float = 1000
+var target_player: CharacterBody2D
+
+func _ready():
+	target_player = get_tree().get_first_node_in_group("player")
 
 func _physics_process(delta):
+	if not target_player:
+		return
 	
+	var movement_angle: Vector2 = position.direction_to(target_player.position)
+	
+	velocity = movement_angle * movement_speed * delta
+	move_and_slide()
